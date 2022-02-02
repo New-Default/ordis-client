a034be563b52d60cec6ab4d9230481d99e9ce8f0
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2022-02-02 22:16:07 +0200

    Row component

---
+++ pages/components/row.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Letter from "./letter";
+
+interface Props {
+  word: string;
+  score: number[] | null;
+}
+
+const Row: React.FC<Props> = ({ word, score }) => {
+  return (
+    <div style={{ height: 100, display: "flex", margin: 10 }}>
+      <Letter character={word[0]} score={score?.[0]} />
+      <Letter character={word[1]} score={score?.[1]} />
+      <Letter character={word[2]} score={score?.[2]} />
+      <Letter character={word[3]} score={score?.[3]} />
+      <Letter character={word[4]} score={score?.[4]} />
+    </div>
+  );
+};
+
+export default Row;
