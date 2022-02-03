c0b27d2ab23cafb30777120a80fab22a92cb1f9f
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2022-02-03 12:53:13 +0200

    Fix row TSC error

--- pages/components/row.tsx
+++ pages/components/row.tsx
@@ -9,11 +9,11 @@ interface Props {
 const Row: React.FC<Props> = ({ word, score }) => {
   return (
     <div style={{ display: "flex", margin: 5 }}>
-      <Letter character={word[0]} score={score?.[0]} />
-      <Letter character={word[1]} score={score?.[1]} />
-      <Letter character={word[2]} score={score?.[2]} />
-      <Letter character={word[3]} score={score?.[3]} />
-      <Letter character={word[4]} score={score?.[4]} />
+      <Letter character={word?.[0]} score={score?.[0]} />
+      <Letter character={word?.[1]} score={score?.[1]} />
+      <Letter character={word?.[2]} score={score?.[2]} />
+      <Letter character={word?.[3]} score={score?.[3]} />
+      <Letter character={word?.[4]} score={score?.[4]} />
     </div>
   );
 };
