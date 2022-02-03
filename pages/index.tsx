d92ef5bd899edda55b8485d751e240f652729a44
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2022-02-03 10:21:42 +0200

    Remove more bloat

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,8 @@ import styles from "../styles/Home.module.css";
 import Board from "./components/board";
 
 const Home: NextPage = () => {
-  
-
   return (
-    <div>
+    <>
       <Head>
         <title>Ordis</title>
         <meta name="description" content="Ordis är ett ordspel" />
@@ -18,17 +16,7 @@ const Home: NextPage = () => {
         <h1>Ordis</h1>
         <Board />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://www.newdefault.se"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ett hobbyprojekt av <span className={styles.logo}>New Default</span>
-        </a>
-      </footer>
-    </div>
+    </>
   );
 };
 
